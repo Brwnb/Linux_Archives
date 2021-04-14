db5859aada859d2fde745675e62a3d0ad81bda65
Bruno Borges <Brwnb@users.noreply.github.com>
2021-04-14 15:18:23 -0300

    Fist commit

---
+++ show_ip_after_boot.sh
@@ -0,0 +1,9 @@
+# Scripts that show ip after linux boot.
+# This script was tested in a CentOS 8 and Debian 10.
+# It's important set the execution permission in this file with command "chmod +x show_ip_after_boot.sh".
+# Other point is that, this file need to stay in directory "/etc/profile.d/"
+
+#Filtering ip
+variable=$(ip a show | grep 'inet ' | sed '1d' | cut -d" " -f6)
+
+echo "ip $variable" > /etc/issue
