8e6bc3f02e84d7a4b50dedd5c011c94b53024e51
Bruno Borges <Brwnb@users.noreply.github.com>
2021-04-14 15:20:41 -0300

    Add note in line 9

--- show_ip_after_boot.sh
+++ show_ip_after_boot.sh
@@ -3,7 +3,8 @@
 # It's important set the execution permission in this file with command "chmod +x show_ip_after_boot.sh".
 # Other point is that, this file need to stay in directory "/etc/profile.d/"
 
-#Filtering ip
+# Filtering ip
 variable=$(ip a show | grep 'inet ' | sed '1d' | cut -d" " -f6)
 
+# set the values in /etc/issue
 echo "ip $variable" > /etc/issue
