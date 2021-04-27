e688d4f86a6ad9d2a989cf449adb4122644a64c7
Bruno Borges <Brwnb@users.noreply.github.com>
2021-04-26 21:26:23 -0300

    Update show_ip_after_boot.sh
    fatou o #!

--- show_ip_after_boot.sh
+++ show_ip_after_boot.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Scripts that show ip after linux boot.
 # This script was tested in a CentOS 8 and Debian 10.
 # It's important set the execution permission in this file with command "chmod +x show_ip_after_boot.sh".
